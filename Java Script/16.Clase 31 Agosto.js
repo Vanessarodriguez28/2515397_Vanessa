6072b9beaf13aca762cc64cb9a78a1d7a50179d4
Vanessarodriguez28 <marilassodiaz18@gmail.com>
2022-09-05 14:48:20 -0500

    primera version

---
+++ Java Script/16.Clase 31 Agosto.js
@@ -0,0 +1,23 @@
+//31-08-2022//
+
+//APUNTES DE LA CLASE(EJERCICIOS)//
+
+//Se puede exportar e importar un codigo a otro archivo
+
+
+//CLASE PERSONA PROTEGIDA //
+/*
+class persona {#
+    nombre
+    constructor
+        (nombre) {
+            this.#nombre = nombre;
+        }
+    get nombre() {
+        return this.#nombre
+    }
+}
+
+let objeto = new persona('Vanessa');
+console.log(objeto.nombre)
+*/
